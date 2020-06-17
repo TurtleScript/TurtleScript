467e10d3a0b7b0b0cc19b1deff10d2a79a1c8a26
amiremohamadi <amiremohamadi@yahoo.com>
2020-06-17 12:33:38 +0430

    read and parse tokens

--- parser/scanner.cc
+++ parser/scanner.cc
@@ -1,6 +1,6 @@
 #include "scanner.hh"
 
-tu::Scanner::Scanner(std::istream *input) {
+tu::Scanner::Scanner(std::istream *input) : yyFlexLexer(input) {
   // Scanner class is not handling memory management and
   // you should be very careful about dangling pointer and memory leaking
 
